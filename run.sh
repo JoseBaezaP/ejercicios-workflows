576cb7ce2f25f3d260d53f9edf48aedd057813ff
jose de la rosa <josedelarosabass@gmail.com>
2021-09-11 02:41:05 -0500

    added the exercise success information

--- run.sh
+++ run.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 z=$(grep -s -L this E-*)
+
+directories=0
+exercises=0
+
 for i in ${z}; 
-do test-runner -e ${i}; 
-done
+do
+  directories=$((directories + 1))
+  test-runner -e ${i} >> result
+  
+  if [ $? -eq 0 ]
+  then
+    exercises=$((exercises + 1))
+  fi
+done;
+
+echo Se resolvieron $exercises ejercicios correctamente de $directories
+
+cat result
